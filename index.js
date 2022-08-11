48d9f3c1fdca37ced6fbb4564133f7bde8b54cb5
Asher Areba <Asher.areba@student.moringaschool.com>
2022-08-11 16:42:15 +0300

    Completed assignemnt

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function introduction(name) {
-    return 'Hi, my name is ${name}.';
-}
+    return `Hi, my name is ${name}.`;
+}    
 function introductionWithLanguage(name, language) {
-    return "Hi, my name is $[name] and I am learning programming in $[language],";
-    }
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
 function introductionWithLanguageOptional(name, language = "JavaScript") {
-    return "Hi, my name is $[name] and I am learning programming in $[language],";
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
